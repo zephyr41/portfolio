289a74ae636dcd917a50f1c155eb2e593da99a5b
Zephyr <nils.jaudon@ynov.com>
2024-08-17 22:58:50 +0200

    rightPageGood

---
+++ script.js
@@ -0,0 +1,8 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  strings: [' hacker éthique <br> <span style="color: #FF6542;"> <strong> + 5k followers ! </strong> </span> ', 'Développeur'],
+  delay: 75,
+  autoStart: true,
+});
