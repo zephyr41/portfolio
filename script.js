a84c8561752adb023417623945f6b3114126709b
Zephyr <nils.jaudon@ynov.com>
2024-09-03 20:46:05 +0200

    portfolio

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
   loop: true,
-  strings: [' hacker éthique <br> <span style="color: #FF6542;"> <strong> + 5k followers ! </strong> </span> ', 'Développeur'],
+  strings: [' hacker éthique <br> <span style="color: #FF6542;">  une communeauté <br> de 6k personnes <br> sur tiktok !  </span> ', 'Développeur'],
   delay: 75,
   autoStart: true,
 });
