e73e059b9ce7b15effc5f8f5fbbe8ed5e583694b
Zephyr <nils.jaudon@ynov.com>
2024-09-14 11:54:21 +0200

    rebase

--- script.js
+++
@@ -1,8 +0,0 @@
-var app = document.getElementById('app');
-
-var typewriter = new Typewriter(app, {
-  loop: true,
-  strings: [' hacker éthique <br> <span style="color: #FF6542;">  une communeauté <br> de 6k personnes <br> sur tiktok !  </span> ', 'Développeur'],
-  delay: 75,
-  autoStart: true,
-});
